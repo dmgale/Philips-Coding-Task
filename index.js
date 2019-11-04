4f73a357580148e8b8f9e579f72bccf5bf19a658
David Gale <davidgale@Davids-MacBook-Pro.local>
2019-11-04 12:38:35 +0000

    serviing static files

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const serve = require('koa-static');
 const Koa = require('koa');
+const path = require('path');
+
 const app = new Koa();
 
-app.use(serve('.'));
+app.use(serve(path.join(__dirname, 'public')));
 
 app.listen(3000, () => console.log('Listening on port 3000'));
