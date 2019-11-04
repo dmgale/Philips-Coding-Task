9845104aee765910618354abbd27d34e74a5cbec
David Gale <davidgale@Davids-MacBook-Pro.local>
2019-11-04 12:51:30 +0000

    Heroku port access

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const app = new Koa();
 
 app.use(serve(path.join(__dirname, 'public')));
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log(`Listening on port ${process.env.PORT || 3000}`));
